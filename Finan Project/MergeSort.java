e11d420a5b0d41a87ec9004a201f73727e675e0f
Yiralle <50154726+Yiralle@users.noreply.github.com>
2021-05-13 21:18:13 -0700

    Add files via upload

---
+++ Finan Project/MergeSort.java
@@ -0,0 +1,59 @@
+package Package;
+
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSort
+{
+	private void merge(int[] leftArr, int[] rightArr, int[]Arr)
+	{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	
+	do  
+	{
+		if(leftArr[a] < rightArr[b])
+		{
+			Arr[c] = leftArr[a];
+			c++; 
+			a++;
+		}
+		else
+		{
+			Arr[c] = rightArr[b];
+			c++; 
+			b++;
+		}
+	} while (a < leftArr.length && b < rightArr.length);
+	
+	while (a < leftArr.length)
+	{
+		Arr[c] = leftArr[a];
+		c++; 
+		a++;
+	}
+	
+	while (b < rightArr.length)
+	{
+		Arr[c] = rightArr[b];
+		c++; 
+		b++;
+	}
+	}
+	
+	
+	private void mergesort(int[] list)
+	{
+		if (list.length <= 1)
+			return;
+		
+		int mid = list.length / 2;
+		int[] leftArr = Arrays.copyOfRange(list, 0, mid);
+		int[] rightArr = Arrays.copyOfRange(list, mid, list.length);
+		
+		mergesort(leftArr);
+		mergesort(rightArr);
+		merge(leftArr,rightArr,list);
+	}
+}
---
+++ Finan Project/QuickSortTask.java
@@ -0,0 +1,75 @@
+package Package;
+
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class QuickSortTask extends RecursiveAction
+{
+	private int [] list;
+	
+	public QuickSortTask(int[] list)
+	{
+		this.list = list;
+	}
+	
+	@Override
+	protected void compute() {
+		// TODO Auto-generated method stub
+		int THRESHOLD = 50;
+		if (list.length >= THRESHOLD) 			
+		{ 
+			
+			return;
+		}
+		else
+		{
+		int mid = list.length / 2;
+		
+		int [] leftsub = Arrays.copyOfRange(list, 0 ,mid);
+		int [] rightsub = Arrays.copyOfRange(list, mid , list.length);
+		
+		QuickSortTask Ltask = new QuickSortTask (leftsub);
+		QuickSortTask Rtask = new QuickSortTask (rightsub);		
+		invokeAll(Ltask,Rtask);
+		quickSort(list, 0, list.length-1);
+	}
+	}
+	
+	public static void quickSort(int list[], int low, int hi)
+    {
+        if (low < hi){
+                    
+            int pivotIndex = partition (list,low,hi);
+  
+            quickSort(list,low,pivotIndex-1);
+            quickSort(list,pivotIndex+1, hi);
+        }
+    }
+	
+	public static int partition (int a[], int i, int j)
+	{ //define pivot and empty arrays
+	       int p = a[i];
+	       int m = i;
+	       
+	       //goes through the unprocessed numbers
+	       for (int k = i+1; k<=j; k++){
+	           if (a[k] < p)
+	           {
+	               m++;
+	            //swap(a[k], a[m]);
+	               int temp;
+	               temp = a[k];
+	               a[k] = a[m];
+	               a[m] = temp;
+	           }
+	           
+	       }
+	            //swap (a[i], a[m]);
+	               int temp2;
+	               temp2 = a[i];
+	               a[i] = a[m];
+	               a[m] = temp2;
+	           return m;
+	 }
+	
+}
