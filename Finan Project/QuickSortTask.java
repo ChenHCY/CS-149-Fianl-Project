b32af9657b115fd11f6e285f7adb4ca231a64e05
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2021-05-14 16:52:38 -0700

    Update QuickSortTask.java

--- Finan Project/QuickSortTask.java
+++ Finan Project/QuickSortTask.java
@@ -18,7 +18,7 @@ public class QuickSortTask extends RecursiveAction
 		int THRESHOLD = 50;
 		if (list.length >= THRESHOLD) 			
 		{ 
-			
+			bubbleSort(list);
 			return;
 		}
 		else
@@ -71,5 +71,19 @@ public class QuickSortTask extends RecursiveAction
 	               a[m] = temp2;
 	           return m;
 	 }
+	 
+	 public static void bubbleSort (int list[])
+    {
+         int n = list.length;
+            for (int i = 0; i < n-1; i++)
+                for (int j = 0; j < n-i-1; j++)
+                    if (list[j] > list[j+1])
+                    {
+
+                        int temp = list[j];
+                        list[j] = list[j+1];
+                        list[j+1] = temp;
+                    }
+    }
 	
 }
